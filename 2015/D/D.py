098427cd39a23134afc16485e98884c4064f7366
VladimirAus <v_roudakov@yahoo.com>
2015-04-12 01:03:21 +1000

    2015.D: Ominous Omino: 2nd attempt

--- 2015/D/D.py
+++ 2015/D/D.py
@@ -1,6 +1,7 @@
 # Input file
-fname = "D-trial"
+# fname = "D-trial"
 # fname = "D-small-attempt0"
+fname = "D-small-attempt1"
 # fname = "D-large"
 inputfile = fname + ".in"
 outputfilename = fname + ".out"
@@ -18,23 +19,23 @@ for nTest in xrange(0, steps):
 	print ((nTest + 1), x, r, c)
 
 	if (x == 1):
-		print ">> x equals 1"
+		print "T: x equals 1"
 		foundSolution = True
 	elif (r * c % x == 0):
 		# there is number of x in r*c
 		if (r >= x) and (c >= x):
-			print ">> can't do longer"
+			print "T: can't do longer"
 			foundSolution = True
 		elif (x >= 7) and (r >= 3) and (c >= 3):
 			pass # RICHARD locks GABRIEL with one cell inside the square
 		else:
 			(a, b) = (1, x)
-			while (a <= b+1):
-				# print (a, b)
+			while (a <= b):
+				print (a, b)
 				foundSolution = True
 				
-				if (((a > c) and (b > c)) or ((a > r) and (b > r))):
-					# print "<< aha"
+				if (((a > c) or (b > r)) and ((b > c) or (a > r))):
+					print "F: can build unfitable"
 					foundSolution = False
 					break
 
