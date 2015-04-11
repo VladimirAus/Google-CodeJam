1f6ae5d03c722162e1864afd7a18d7e2f3d261f7
VladimirAus <v_roudakov@yahoo.com>
2015-04-12 07:32:16 +1000

    2015.B: House of Pancakes: initial solution

--- 2015/B/B.py
+++ 2015/B/B.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # Input file
 fname = "B-trial"
 # fname = "B-small-attempt0"
@@ -13,8 +15,32 @@ result = list()
 # print lines
 # Solution
 
+def splitPancakes( num ):
+	if (num % 2 == 0):
+		return (num/2, num/2)
+	else:
+		return ((num-1)/2, ((num-1)/2)+1)
+
 for nTest in xrange(0, steps):
-	result.append(0)
+	specialMinutes = 0
+	
+	dinners = int (lines[2 * nTest])
+	pancakes = sorted(map(int, lines[2 * nTest + 1].split(" ")))
+	print (nTest+1, dinners, pancakes)
+
+	# minuteTaken = 0
+	while (max(pancakes) > 3):
+		currentSplit = splitPancakes(max(pancakes))
+		if (currentSplit[1] + specialMinutes + 1 < max(pancakes) + specialMinutes):
+			specialMinutes += 1
+			pancakes[len(pancakes)-1] = currentSplit[0]
+			pancakes.append(currentSplit[1])
+			sorted(pancakes)
+			print ("N:", pancakes)
+		else:
+			break
+
+	result.append(max(pancakes) + specialMinutes)
 
 # Output file
 # print result
