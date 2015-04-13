0803ad963a740320b5b9e0b69b851ed39ef9f166
VladimirAus <v_roudakov@yahoo.com>
2015-04-13 12:42:47 +1000

    2015.B: House of Pancakes: final attempts

--- 2015/B/B.py
+++ 2015/B/B.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
 # Input file
-fname = "B-trial"
+# fname = "B-trial"
 # fname = "B-small-attempt0"
+# fname = "B-small-attempt1"
+# fname = "B-small-attempt2"
+# fname = "B-small-attempt3"
+fname = "B-small-attempt4"
 # fname = "B-large"
 inputfile = fname + ".in"
 outputfilename = fname + ".out"
@@ -23,27 +27,30 @@ def splitPancakes( num ):
 
 for nTest in xrange(0, steps):
 	specialMinutes = 0
+	splitResults = list()
 	
 	dinners = int (lines[2 * nTest])
 	pancakes = sorted(map(int, lines[2 * nTest + 1].split(" ")))
 	print (nTest+1, dinners, pancakes)
 
 	# minuteTaken = 0
+	splitResults.append(max(pancakes))
 	while (max(pancakes) > 3):
 		currentSplit = splitPancakes(max(pancakes))
-		if (currentSplit[1] + specialMinutes + 1 < max(pancakes) + specialMinutes):
-			specialMinutes += 1
-			pancakes[len(pancakes)-1] = currentSplit[0]
-			pancakes.append(currentSplit[1])
-			sorted(pancakes)
-			print ("N:", pancakes)
-		else:
-			break
+		# if (currentSplit[1] + specialMinutes + 1 < max(pancakes) + specialMinutes):
+		specialMinutes += 1
+		pancakes[len(pancakes)-1] = currentSplit[0]
+		pancakes.append(currentSplit[1])
+		pancakes = sorted(pancakes)
+		splitResults.append(max(pancakes) + specialMinutes)
+		print ("N:", pancakes, specialMinutes, max(pancakes) + specialMinutes, min(splitResults))
+		# else:
+		# 	break
 
-	result.append(max(pancakes) + specialMinutes)
+	result.append(min(splitResults))
 
 # Output file
 # print result
 outputfile = open(outputfilename, 'w')
 for nCase in xrange(0, len(result)):
-	outputfile.write("Case #%d: %d \n" % ((nCase + 1), (result[nCase])) )
+	outputfile.write("Case #%d: %d\n" % ((nCase + 1), (result[nCase])) )
